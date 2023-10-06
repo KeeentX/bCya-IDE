32c4523f7801cb6bf8ec669d89dd24dc0153b5d9
Kent Dave Catipay <kpcatipay@up.edu.ph>
2023-10-06 12:38:16 +0800

    feat(layout): add nextjs layout file

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import React from "react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BCYA IDE',
+  description: 'BCYA COMPILER IDE',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`bg-primary-dark ${inter.className}`}>{children}</body>
+    </html>
+  )
+}
