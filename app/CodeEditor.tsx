0bc2897e1bc671d0731b52800abd5abe0d61dfec
Kent Dave Catipay <kpcatipay@up.edu.ph>
2023-10-19 17:32:22 +0800

    feat(code editor): change theme, add change listener and save state updater

--- app/CodeEditor.tsx
+++ app/CodeEditor.tsx
@@ -1,16 +1,32 @@
 "use client"
 import CodeMirror from '@uiw/react-codemirror';
-import {vscodeDark} from "@uiw/codemirror-theme-vscode";
+import {tokyoNight} from "@uiw/codemirror-themes-all";
 import {cpp} from "@codemirror/lang-cpp";
+import {OpenedFile, OpenedFilesContext} from "@/app/context/FileContext";
+import {useContext} from "react";
 
 export default function CodeEditor(props: any) {
+    const {openedFiles, setOpenedFiles} = useContext(OpenedFilesContext)
     return (
         <CodeMirror
             value={props.content}
-            theme={vscodeDark}
-            extensions={[cpp()]}
+            theme={tokyoNight}
+            extensions={[
+                cpp()
+            ]}
             height={props.height}
-            width="80dvw"
+            width="85dvw"
+            onChange={(editor) => {
+                openedFiles[props.index].saved ?
+                    setOpenedFiles((prevState: OpenedFile[]) => {
+                        prevState[props.index].saved = false;
+                        return [...prevState];
+                    }) : null;
+                setOpenedFiles((prevState: OpenedFile[]) => {
+                    prevState[props.index].newContent = editor;
+                    return [...prevState];
+                });
+            }}
         />
     )
 }
