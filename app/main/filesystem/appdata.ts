e5fbb77ebcc2fc726e2bafd68593e99d0db53324
Kent Dave Catipay <kpcatipay@up.edu.ph>
2023-10-20 07:27:13 +0800

    fix(basedirectory): chage basedirectory to document

--- app/main/filesystem/appdata.ts
+++ app/main/filesystem/appdata.ts
@@ -2,7 +2,7 @@ import { writeTextFile, BaseDirectory } from '@tauri-apps/api/fs';
 
 export async function write(location: string, content: string) {
     try {
-        await writeTextFile(location, content, { dir: BaseDirectory.Home });
+        await writeTextFile(location, content, { dir: BaseDirectory.Document });
     } catch (error) {
         console.error(error);
         return false;
