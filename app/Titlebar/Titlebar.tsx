83fc45c532dc28a9ddb598403d619d266c09b8d9
Kent Dave Catipay <kpcatipay@up.edu.ph>
2023-10-20 07:48:32 +0800

    perf(titlebar): optimize titlebar for main screen

--- app/Titlebar/Titlebar.tsx
+++ app/Titlebar/Titlebar.tsx
@@ -1,11 +1,10 @@
 "use client"
-import { appWindow } from '@tauri-apps/api/window'
+
 import React from "react";
-import TitleBarMenu from "@/app/Titlebar/TitlebarMenu";
+import TitleBarMenu from "@/app/main/Titlebar/TitlebarMenu";
 import {NextFont} from "next/dist/compiled/@next/font";
 import {Coda} from "next/font/google";
-import {CloseIcon, MaximizeIcon, MinimizeIcon} from "@/app/components/icons";
-import {Icon} from "@iconify/react";
+import TitleBarActions from "@/app/Titlebar/TitleBarActions";
 
 const coda : NextFont = Coda({
     style: 'normal',
@@ -13,11 +12,6 @@ const coda : NextFont = Coda({
     weight: "400",
 });
 export default async function TitleBar() {
-    const [isMaximized, setIsMaximized] = React.useState(false);
-    await appWindow.onResized(async () => {
-        const maximized = await appWindow.isMaximized();
-        setIsMaximized(maximized);
-    });
     return (
         <div data-tauri-drag-region="" className={"bg-primary-dark flex flex-row items-center justify-between m-2"}>
             <TitleBarMenu coda={coda}/>
@@ -25,32 +19,7 @@ export default async function TitleBar() {
                   data-tauri-drag-region="">
                 BCYA IDE V0.2.0
             </span>
-            <div className={'flex flex-row'}>
-                <div
-                    className={"p-2 hover:bg-gray-500 cursor-pointer rounded-full"}
-                    onClick={() => appWindow.minimize()}>
-                    <MinimizeIcon width={'12'} height={'12'} title={'Minimize IDE'}/>
-                </div>
-                <div
-                    className={"p-2 hover:bg-gray-500 cursor-pointer rounded-full"}
-                    onClick={() => {
-                        isMaximized ? appWindow.unmaximize() : appWindow.maximize()
-                        setIsMaximized(prevState => !prevState)
-                    }}>
-                    {isMaximized ?
-                        <Icon
-                            icon={'fluent:full-screen-minimize-20-regular'}
-                            width={'12'}
-                            height={'12'}
-                            className={'stroke-white'}/> :
-                        <MaximizeIcon width={'12'} height={'12'} title={'Maximize IDE'}/>}
-                </div>
-                <div
-                    className="p-2 hover:bg-red-400 cursor-pointer rounded-full"
-                    onClick={() => appWindow.close()}>
-                    <CloseIcon width={'12'} height={'12'} title={'Close IDE'}/>
-                </div>
-            </div>
+            <TitleBarActions/>
         </div>
     )
 }
