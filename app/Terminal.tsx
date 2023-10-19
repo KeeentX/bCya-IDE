6378df222678d89e8eef6492c9d109671f897334
Kent Dave Catipay <kpcatipay@up.edu.ph>
2023-10-19 17:43:54 +0800

    revert(terminal): remove tauri invoke for terminal shell

--- app/Terminal.tsx
+++ app/Terminal.tsx
@@ -1,14 +1,17 @@
 "use client"
 import {Terminal} from "xterm";
 import {useEffect} from "react";
-import {invoke} from "@tauri-apps/api/tauri";
+import {Command} from "@tauri-apps/api/shell";
 
 interface Response {
     current_dir: string,
     stdout: string,
     stderr: string,
 }
-export default function TerminalComponent() {
+export default async function TerminalComponent(props: { showTerminal: boolean }) {
+    // const command = new Command('cmd.exe')
+    // const child = await command.spawn()
+    // console.log(child)
     useEffect(() => {
         const element = document.getElementById('terminal');
         if (element)
@@ -22,21 +25,9 @@ export default function TerminalComponent() {
         term.open(element as HTMLElement);
         term.write(__dirname + ' $ ')
         term.onKey((e) => {
-            if(e.key === '\r') {
+            if (e.key === '\r') {
                 // When the user presses enter, we invoke the command
-                term.write('\r\n')
-                invoke<Response>('spawn_terminal', {command: command})
-                    .then((response) => {
-                        term.write(response["stdout"] as string)
-                        term.write(response["stderr"] as string)
-                        term.write('\r\n')
-                        term.write(response["current_dir"] as string + " $ ")
-                    })
-                    .catch(error => {
-                        term.write(error as string)
-                        term.write('\r\n')
-                    });
-                command = "";
+                term.write('\r\n');
             } else if (e.key === '\x7f') {
                 // When the user presses backspace, we remove the last character
                 term.write('\b \b');
@@ -50,6 +41,6 @@ export default function TerminalComponent() {
     }, []);
 
     return (
-        <div id={"terminal"} className={'w-[80dvw]'}></div>
+        <div id={"terminal"} className={`w-[100dvw] ${props.showTerminal ? null : 'hidden'}`}></div>
     )
 }
